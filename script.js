2251571ee2043f6ba8597f25b93bcb6d78c8416b
Aisha mohammed <hayunadus111@gmail.com>
2024-04-23 23:33:09 +0300

    Update the method of storing the varidables

--- script.js
+++ script.js
@@ -27,34 +27,54 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     // Target = event.target;
     if (
-      !(
-        button.id == "minus" ||
-        button.id == "add" ||
-        button.id == "div" ||
-        button.id == "pro" ||
-        button.id == "mod" ||
-        button.id == "AC" ||
-        button.id == "del" ||
-        button.id == "equal"
-      )
-    ) {
-      let val = button.id;
-      span.textContent = val;
-      variable = parseInt(val, 10);
-      storeNum = storeVar(variable);
-      console.log(storeNum);
-    }
-    if (
+      button.id == "0" ||
+      button.id == "1" ||
+      button.id == "2" ||
+      button.id == "3" ||
+      button.id == "4" ||
+      button.id == "5" ||
+      button.id == "6" ||
+      button.id == "7" ||
+      button.id == "8" ||
+      button.id == "9" ||
       button.id == "minus" ||
       button.id == "add" ||
       button.id == "div" ||
       button.id == "pro" ||
-      button.id == "mod"
+      button.id == "mod" ||
+      button.id == "AC" ||
+      button.id == "del" ||
+      button.id == "equal"
     ) {
-      op = button.id;
-      storeOpr = storeOp(op);
-      console.log(storeOpr);
+      let val = button.id;
+      array.push(val);
+      firstNum = array[0];
+      for (let i = 1; i < array.length; i++) {
+        if (typeof array[i] === "number") {
+          firstNum += array[i];
+
+          span.textContent = firstNum;
+          variable = parseInt(firstNum, 10);
+          storeNum = storeVar(variable);
+          console.log(storeNum);
+        } else {
+          op = button.id;
+          storeOpr = storeOp(op);
+          console.log(storeOpr);
+        }
+      }
     }
+    // if (
+    //   button.id == "minus" ||
+    //   button.id == "add" ||
+    //   button.id == "div" ||
+    //   button.id == "pro" ||
+    //   button.id == "mod"
+    // ) {
+    //   op = button.id;
+    //   storeOpr = storeOp(op);
+    //   console.log(storeOpr);
+    // }
     if (button.id == "AC") {
       span.textContent = "";
       array = [];
