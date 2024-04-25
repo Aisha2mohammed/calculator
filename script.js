1ee4fdbeac426861798addce15fb3d4ec5915ff1
Aisha mohammed <hayunadus111@gmail.com>
2024-04-25 17:30:34 +0300

    Update the rendering method

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const container = document.querySelector(".container");
 const buttons = document.querySelectorAll("button");
-// const display = document.querySelector('.display');
-
 const span = document.querySelector(".span1");
-// const span2 = document.querySelector(".span2");
-
 const equal_button = document.querySelector("#equal");
 
 let array = [];
@@ -52,65 +48,67 @@ container.addEventListener("click", (event) => {
       variable = "div";
       display(variable);
       break;
-    case "one":
+    case "1":
       span.textContent = "1";
       variable = 1;
       display(variable);
       break;
 
-    case "two":
+    case "2":
       span.textContent = "2";
       variable = 2;
       display(variable);
       break;
-    case "three":
+    case "3":
       span.textContent = "3";
       variable = 3;
       display(variable);
       break;
-    case "four":
+    case "4":
       span.textContent = "4";
       variable = 4;
       display(variable);
       break;
-    case "five":
+    case "5":
       span.textContent = "5";
       variable = 5;
       display(variable);
       break;
-    case "six":
+    case "6":
       span.textContent = "6";
       variable = 6;
       display(variable);
       break;
-    case "seven":
+    case "7":
       span.textContent = "7";
       variable = 7;
       display(variable);
       break;
 
-    case "eight":
+    case "8":
       span.textContent = "8";
       variable = 8;
       display(variable);
       break;
-    case "nine":
+    case "9":
       span.textContent = "9";
       variable = 9;
       display(variable);
       break;
-    case "zero":
+    case "0":
       span.textContent = "0";
       variable = 0;
       display(variable);
       break;
+    case "equal":
+      result = operate(storeOpr, storeNum);
+      span.textContent = result;
+      variable = result;
+      display(variable);
+      break;
     default:
       span.textContent = "invalid input";
-    // case "point":
-    //   span.textContent = ".";
-    //   variable = ".";
-    //   display(variable);
-    //   break;
+
     // // case 'plus_minus':
     // span.textContent = '.';
     // variable = . ;
@@ -130,12 +128,17 @@ function display(numbers) {
       span.textContent = concat;
       console.log(concat);
       storeNum = storeVar(concat);
-    } else {
-      op = arr[i];
+    }
+    if (typeof array[i] === "string") {
+      op = array[i];
       concat = "";
       storeOpr = storeOp(operator);
     }
   }
+  // equal_button.addEventListener("click", function (storeOpr, storeNum) {
+  //   result = operate(storeOpr, storeNum);
+  //   span.textContent = result;
+  // });
 }
 
 function storeVar(variable) {
@@ -144,15 +147,22 @@ function storeVar(variable) {
   arr = array.map((el) => parseInt(el, 10));
   return arr;
 }
-function storeOp(operator) {
-  op = operator;
-  console.log(op);
-  return op;
+function storeOp(name) {
+  operator = name;
+  console.log(operator);
+
+  if (
+    operator === "add" ||
+    operator === "minus" ||
+    operator === "pro" ||
+    operator === "mod" ||
+    operator === "div"
+  ) {
+    // console.log(operator);
+    return operator;
+  } else return undefined;
 }
-equal_button.addEventListener("click", function (storeOpr, storeNum) {
-  result = operate(storeOpr, storeNum);
-  span.textContent = result;
-});
+
 function operate(storeOpr, storeNum) {
   if (storeOpr == "add") return add(storeNum);
 
